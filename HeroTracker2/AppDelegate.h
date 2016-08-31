6c13e541e3e930ecafa0a344504c017c95d90122
Komari Herring <komherr@gmail.com>
2016-08-30 21:50:25 -0400

    Initial Commit

---
+++ HeroTracker2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeroTracker2
+//
+//  Created by Komari Herring on 8/30/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
